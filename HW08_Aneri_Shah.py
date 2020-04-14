9a46042fe550529894fa8bdf9f7d25831322e412
aneri2710 <31449617+aneri2710@users.noreply.github.com>
2020-04-14 19:49:18 -0400

    HW 09

---
+++ HW08_Aneri_Shah.py
@@ -0,0 +1,88 @@
+"""
+Aneri Shah
+Homework 8
+
+Implementing different functions to implement different python modules such as datetime, prettytable, os
+
+"""
+
+from datetime import datetime, timedelta
+import os
+from typing import Tuple, Iterator, Dict, List
+from prettytable import PrettyTable
+
+
+def date_arithmetic() -> Tuple[datetime, datetime, int]:
+    """ Implementing datetime module for returning of dates after some specific period of time """
+    three_days_after_02272000: datetime = datetime(2020, 2, 27) + timedelta(days=3)
+
+    three_days_after_02272017: datetime = datetime(2019, 2, 27) + timedelta(days=3)
+
+    date3 = datetime(2019, 2, 1)
+    date4 = datetime(2019, 9, 30)
+    days_passed_01012017_10312017: int = date4 - date3
+
+    return three_days_after_02272000, three_days_after_02272017, days_passed_01012017_10312017.days
+
+
+def file_reader(path, fields, sep='|', header=False) -> Iterator[Tuple[str]]:
+    """ Implementing a file reader function which allows to read a file and returns the number of fields which are
+    separated by a separator """
+    try:
+        filePath = open(path, "r")
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Cant open file {path}")
+    else:
+        with filePath:
+            if filePath != "":
+                for n, line in enumerate(filePath, 1):
+                    result:List[str] = line.rstrip('\n').split(sep)
+                    if len(result) != fields:
+                        raise ValueError(f" {filePath} line: {n}: read {len(result)} fields but expected {fields} ")
+                        # yield tuple(line.strip().split(sep))
+                    elif n > 1 or header is False:
+                        yield result
+
+
+class FileAnalyzer:
+    """ Class FileAnalyzer is a class which implements different functions to get the number of lines, characters,
+    functions and classes in a directory with .py file """
+
+    def __init__(self, directory: str) -> None:
+        """ Initializing the directory and file summary of the class """
+        self.directory: str = directory  # NOT mandatory!
+        self.files_summary: Dict[str, Dict[str, int]] = dict()
+
+        self.analyze_files()  # summerize the python files data
+
+    def analyze_files(self) -> None:
+        """ This will search the file ending with .py and counts the number of  lines, characters, unctions and
+        classes and adding it in files.summary"""
+        for files in os.listdir(self.directory):
+            if files.endswith('.py'):
+                classno, lines, funcno, charno = 0, 0, 0, 0
+                try:
+                    f = open(os.path.join(self.directory, files), 'r')
+                except FileNotFoundError:
+                    raise FileNotFoundError
+                else:
+                    for lineno in f.readlines():
+                        lines += 1
+                        if lineno.strip().startswith('def '):
+                            funcno += 1
+                        if lineno.strip().startswith('class '):
+                            classno += 1
+                        charno += len(lineno)
+                    self.files_summary[files] = {"class": classno, "function": funcno, "line": lines, "char": charno}
+                    f.close()
+
+    def pretty_print(self) -> None:
+        """ This will print the pretty table of the field names  """
+        pretty_table = PrettyTable()
+        pretty_table.field_names = ["File Name", "Classes", "Functions", "Lines", "Characters"]
+
+        for values in self.files_summary.items():
+            pretty_table.add_row([values, values["class"], values["function"],
+                                  values["line"], values["char"]])
+
+        return pretty_table
--- Student_Repository_Aneri_Shah.py
+++ Student_Repository_Aneri_Shah.py
@@ -1,9 +1,9 @@
 """
 Aneri Shah
-Homework 10
+Homework 9
 
-Implementing student, instructor, grades, major and repository for entering and processing all the student data,
-instructor data and the student's grade along with the major of that student"""
+Implementing student, instructor, grades, and repository for entering and processing all the student data,
+instructor data and the student's grade """
 
 import os
 from collections import defaultdict
@@ -14,92 +14,30 @@ from prettytable import PrettyTable
 from HW08_Aneri_Shah import file_reader
 
 
-class Major:
-    """ This is a Major class for adding the majors based on required and elective courses in the majors.txt. """
-    PT_FIELD_NAMES = ["Major", "Required", "Electives"]
-
-    def __init__(self, major: str) -> None:
-        """ Initializes the major, required and elective courses taken in protected form. """
-        self._major = major
-        self._required = set()
-        self._elective = set()
-
-    def add_course_re(self, flag, course: str) -> None:
-        """ Checks if the course is required or elective from the majors.txt file """
-        if flag.lower() == 'r':
-            self._required.add(course)
-        elif flag.lower() == 'e':
-            self._elective.add(course)
-        else:
-            raise ValueError(f"Unknown Flag: {flag}")
-
-    def remaining(self, courses):
-        """ Checks for the grades in the list of grades.
-        Returns the remaining number of courses which can be remaining or elective """
-        completed_courses = set()
-        remaining_required = self._required.copy()
-        remaining_elective = self._elective.copy()
-
-        for course, grade in courses.items():
-            if grade in ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C']:
-                completed_courses.add(course)
-                if course in self._elective:
-                    remaining_elective = []
-
-            if course in self._required and grade in ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C']:
-                remaining_required.remove(course)
-
-        return completed_courses, remaining_required, remaining_elective
-
-    def info(self):
-        """return a list of the information about the courses needed for the pretty table"""
-        return [self._major, sorted(self._required), sorted(self._elective)]
-
-
 class Student:
-    """ This provides information of the individual student which includes CWID, Name, Major, the courses which are completed, remaining and elective and their gpa.
+    """ This provides information of the individual student which includes CWID, Name, Major, and the courses taken by them
     This will return individual student information for the repository class."""
-    PT_FIELD_NAMES = ['CWID', 'Name', 'Major', 'Completed Courses', 'Remaining Required', 'Remaining Electives', 'GPA']
+    PT_FIELD_NAMES = ['CWID', 'Name', 'Major', 'Courses']
 
     def __init__(self, cwid: str, name: str, major: str) -> None:
-        """ Initializes the cwid, name, major, and courses taken in protected form along with the completed courses, remaining courses and elective courses """
+        """ Initializes the cwid, name, major, and courses taken in protected form. """
         self._cwid: str = cwid
         self._name: str = name
         self._major: str = major
         self._courses: Dict[str, str] = dict()
 
-        self._completed_courses = None
-        self._remaining_required = None
-        self._remaining_electives = None
-
     def add_course_grade(self, course: str, grade: str) -> None:
         """ This is to give the particular course taken by the student the grades earned by them """
         self._courses[course] = grade
 
-    def gpa(self):
-        """calculate the GPA and return to student class"""
-        grades: Dict[str, float] = {"A": 4.00, "A-": 3.75, "B+": 3.25, "B": 3.00, "B-": 2.75, "C+": 2.25, "C": 2.00, "C-": 0.00, "D+": 0.00, "D": 0.00, "D-": 0.00, "F": 0.00}
-        try:
-            total: float = sum([grades[grade] for grade in self._courses.values()]) / len(self._courses.values())
-            return round(total, 2)
-        except ZeroDivisionError as e:
-            print(e)
-
-    def course_re(self, inst_major):
-        """ This will see for the courses in the major class by calling the remaining courses of the major class. """
-        self._completed_courses, self._remaining_required, self._remaining_electives = inst_major.remaining(
-            self._courses)
-
     def info(self):
         """return a list of the information about individual student needed for the pretty table"""
-        return [self._cwid, self._name, self._major, sorted(self._completed_courses), sorted(self._remaining_required),
-                sorted(self._remaining_electives), self.gpa()]
+        return [self._cwid, self._name, self._major, sorted(self._courses.keys())]
 
 
 class Instructor:
     """ This provides information of the instructor which includes CWID, Name, Department, and the courses they teach
     This will return the instructor information for the repository class. """
-
     PT_FIELD_NAMES = ["CWID", "Name", "Dept", "Courses", "Students"]
 
     def __init__(self, cwid: str, name: str, dept: str) -> None:
@@ -121,65 +59,43 @@ class Instructor:
 
 
 class Repository:
-    """ This class holds all the information of all the students, all the instructors, the grades earned by the students, and the major courses taken by the students.
+    """ This class holds all the information of all the students, all the instructors, and the grades earned by the students.
     This class will create the instance of student, instructor and then print the pretty table according to their field names."""
 
     def __init__(self, path: str, ptable=False) -> None:
-        """store all students, instructors, majors and print prettytables """
+        """store all students, instructors, and print prettytables """
         if not os.path.exists(path):
             raise FileNotFoundError(f'{path} not found')
         self.path: str = path
         self._students: Dict[str, Student] = dict()
         self._instructors: Dict[str, Instructor] = dict()
-        self._major: Dict[str, Major] = dict()
         try:
-            self._read_major(os.path.join(path, "majors.txt"))
             self._read_students(os.path.join(path, "students.txt"))
             self._read_instructors(os.path.join(path, "instructors.txt"))
             self._read_grades(os.path.join(path, "grades.txt"))
-            self._read_re()
         except (FileNotFoundError, ValueError) as e:
             print(e)
 
         if ptable:
-            self.major_pretty_table()
             self.student_pretty_table()
             self.instructor_pretty_table()
-
-    def _read_major(self, path: str) -> None:
-        """ read each line from majors file and create instance of class major
-        and if the file not found or any value error, it raises an exception """
-        try:
-            for major, req, course in file_reader(path, 3, sep='\t', header=True):
-                if major not in self._major:
-                    self._major[major] = Major(major)
-
-                self._major[major].add_course_re(req, course)
-
-        except (FileNotFoundError, ValueError) as e:
-            print(e)
 
     def _read_students(self, path: str) -> None:
         """ read each line from student file and create instance of class student
         and if the file not found or any value error, it raises an exception """
         try:
-            for cwid, name, major in file_reader(path, 3, sep=';', header=True):
-                if cwid not in self._students:
-                    self._students[cwid] = Student(cwid, name, major)
-                else:
-                    print(f" Duplicate student {cwid}")
+            for cwid, name, major in file_reader(path, 3, sep='\t', header=False):
+                self._students[cwid] = Student(cwid, name, major)
+
         except (FileNotFoundError, ValueError) as e:
             print(e)
 
-    def _read_instructors(self, path: str):
+    def _read_instructors(self, path:str):
         """read each line from instructors file and create instance of class instructor
         and if the file not found or any value error, it raises an exception"""
         try:
-            for cwid, name, dept in file_reader(path, 3, sep='|', header=True):
-                if cwid not in self._instructors:
-                    self._instructors[cwid] = Instructor(cwid, name, dept)
-                else:
-                    print(f" Duplicate instructor {cwid}")
+            for cwid, name, dept in file_reader(path, 3, sep='\t', header=False):
+                self._instructors[cwid] = Instructor(cwid, name, dept)
 
         except (FileNotFoundError, ValueError) as e:
             print(e)
@@ -189,7 +105,7 @@ class Repository:
         and checks for the individual student and the individual instructor.
         Raises an exception when unknown student is found"""
         try:
-            for student_cwid, course, grade, instructor_cwid in file_reader(path, 4, sep='|', header=True):
+            for student_cwid, course, grade, instructor_cwid in file_reader(path, 4, sep='\t', header=False):
                 try:
                     s: Student = self._students[student_cwid]
                     s.add_course_grade(course, grade)
@@ -202,26 +118,6 @@ class Repository:
                     print(f"Found grade for unknown student {instructor_cwid}")
         except (FileNotFoundError, ValueError) as e:
             print(e)
-
-    def _read_re(self):
-        """ checking for the cwid in the major which is not in the repository """
-        for cwid in self._students:
-            student_info = self._students[cwid]
-            student_major = student_info._major
-
-            try:
-                student_info.course_re(self._major[student_major])
-            except:
-                print(f" Student with CWID {cwid}  has a major {student_major} not in repository")
-
-    def major_pretty_table(self) -> None:
-        """print pretty table with major information by calling the global constant."""
-        pt = PrettyTable(field_names=Major.PT_FIELD_NAMES)
-        for mj in self._major.values():
-            pt.add_row(mj.info())
-
-        print("Major Summary")
-        print(pt)
 
     def student_pretty_table(self) -> None:
         """print pretty table with student information by calling the global constant."""
@@ -245,7 +141,7 @@ class Repository:
 
 def main():
     """ define a repository for stevens"""
-    stevens: Repository = Repository("/Users/anerishah/Desktop/SSW810/WEEK 10", ptable=True)
+    stevens: Repository = Repository("/Users/anerishah/Desktop/SSW810/WEEK 9", ptable= True)
 
 
 if __name__ == '__main__':
